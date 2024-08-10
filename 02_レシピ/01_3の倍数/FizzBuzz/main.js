d838425fe70d894c78ed7a68f7d613741d95ea0f
same04 <sksamebu68@gmail.com>
2024-08-10 19:46:49 +0900

    [FizzBuzz]を学習しました。

---
+++ 02_レシピ/01_3の倍数/FizzBuzz/main.js
@@ -0,0 +1,13 @@
+const FizzBuzz = function (number) {
+  for (let n = 1; n <= number; n++) {
+    if (n % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (n % 3 === 0) {
+      console.log("Fizz")
+    } else if (n % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(n)
+    }
+  }
+}
